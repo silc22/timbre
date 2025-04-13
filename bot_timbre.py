20b80e539fbc25a069bd9172348b153d0b6ee2ab
silc22 <silcerquatti@gmail.com>
2025-04-13 16:24:18 +0200

    first commit

---
+++ bot_timbre.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+# Tu TOKEN y tu chat_id (Telegram user ID)
+TOKEN = '7695311258:AAGUf6wVkPQZeix9nB09-0VdG-LmPW6jCdg'
+CHAT_ID = '@TimbreSil_bot'
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        if text == "/start":
+            send_msg(chat_id, "¡Hola! Este es el timbre. Tocá el botón para avisar que estás abajo.")
+            send_button(chat_id)
+
+        elif text == "🚪 Tocar timbre":
+            send_msg(CHAT_ID, "🚨 Tocaron el timbre abajo.")
+
+    return "ok", 200
+
+def send_msg(chat_id, text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {"chat_id": chat_id, "text": text}
+    requests.post(url, json=data)
+
+def send_button(chat_id):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": chat_id,
+        "text": "Tocá el botón para avisarme:",
+        "reply_markup": {
+            "keyboard": [[{"text": "🚪 Tocar timbre"}]],
+            "resize_keyboard": True,
+            "one_time_keyboard": True
+        }
+    }
+    requests.post(url, json=data)
+
